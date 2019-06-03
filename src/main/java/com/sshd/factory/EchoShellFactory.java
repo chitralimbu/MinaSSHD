e92371994e92a07e50bd29a9b5501fd9509d2206
Chitra Limbu <limbu_nikkil@hotmail.co.uk>
2019-06-03 21:47:52 +0100

    ssh working

---
+++ src/main/java/com/sshd/factory/EchoShellFactory.java
@@ -0,0 +1,84 @@
+package com.sshd.factory;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+
+import org.apache.sshd.common.Factory;
+import org.apache.sshd.server.Command;
+import org.apache.sshd.server.Environment;
+import org.apache.sshd.server.ExitCallback;
+
+public class EchoShellFactory implements Factory<Command>{
+
+	public Command create() {
+		return new EchoShell();
+	}
+	
+	@SuppressWarnings("unused")
+	public static class EchoShell implements Command, Runnable{
+		private InputStream in;
+		private OutputStream out;
+		private OutputStream err;
+		private ExitCallback callback;
+		private Environment environment;
+		private Thread thread;
+		
+		public void start(Environment env) throws IOException {
+			environment = env;
+			thread = new Thread(this, "EchoShell");
+			thread.start();
+		}
+
+		public void destroy() throws Exception {
+			thread.interrupt();
+			
+		}
+
+		public void run() {
+			BufferedReader r = new BufferedReader(new InputStreamReader(in));
+			try {
+				for(;;) {
+					String s = r.readLine();
+					if(s == null) {
+						return;
+					}
+					out.write((s + "\n").getBytes());
+					out.flush();
+					if("exit".equals(s)) {
+						return;
+					}
+				}
+			}catch(Exception e) {
+				e.printStackTrace();
+			}finally {
+				callback.onExit(0);
+			}
+			
+		}
+
+		public void setInputStream(InputStream in) {
+			this.in = in;
+			
+		}
+
+		public void setOutputStream(OutputStream out) {
+			this.out = out;
+			
+		}
+
+		public void setErrorStream(OutputStream err) {
+			this.err = err;
+			
+		}
+
+		public void setExitCallback(ExitCallback callback) {
+			this.callback = callback;
+			
+		}
+		
+	}
+	
+}
---
+++ src/test/java/com/sshd/test/Test.java
@@ -0,0 +1,10 @@
+package com.sshd.test;
+
+public class Test {
+	
+	public static void main(String[] args) {
+		
+		System.out.println(System.getProperty("os.name"));
+		
+	}
+}
